a4696e8fe2ec958d2f47fa7b012dc2253a04da03
MatthewMacalaladGBC <matthew.macalalad@georgebrown.ca>
2026-04-11 13:44:46 -0400

    Initialized new Angular frontend and added assignment 1 backend to root

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js"
+import typeDefs from "./schemas/typeDefs.js";
+import resolvers from "./resolvers/resolvers.js";
+
+import { ApolloServer }  from "@apollo/server";
+import { expressMiddleware } from "@as-integrations/express5";
+
+const app = express();
+
+async function startServer() {
+    await connectDB();
+
+    app.use(express.json({ limit: '10mb' }));
+    app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
+    // Define Apollo Server
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers
+    });
+
+    // Start the Apollo Server
+    await server.start();
+
+    // Apply middleware to the Express app
+    app.use(
+      "/graphql", 
+      cors(),
+      express.json(),
+      expressMiddleware(server)
+    );
+
+    //Start Express server
+    app.listen(process.env.PORT, () => {
+      console.log(`🚀 Server ready at http://localhost:${process.env.PORT}/graphql`);
+      //Connect to MongoDB Atlas
+    });
+}
+
+startServer();
